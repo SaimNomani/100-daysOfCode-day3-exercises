69c3a5d54a39cbc0a4d0a5727266d61404719b0e
SaimNomani <nomanisaim591@gmail.com>
2023-09-30 02:24:16 +0500

    exercise1: odd/even

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+#
+# Write a program that works out whether if a given number is an odd or even number.
+#
+number = int(input("Enter number: "))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
